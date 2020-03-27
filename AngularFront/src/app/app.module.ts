f89bf471361ccfc83832a5621ea738c2476c25a8
adarribe <adrien.darribeyros@etud.univ-jfc>
2020-03-27 12:58:57 +0100

    ajout du boostrap et modification de quelques classes

--- AngularFront/src/app/app.module.ts
+++ AngularFront/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RestserviceService } from './restservice.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ProductComponent } from './product/product.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [RestserviceService],
   bootstrap: [AppComponent]
--- AngularFront/src/app/world.ts
+++ AngularFront/src/app/world.ts
@@ -1,18 +1,126 @@
 
 export class World {
-    name : string; 
-    logo : string;
-    money: number; 
-    score: number; 
-    totalangels: number;
-    activeangels: number;
-    angelbonus: number;
-    lastupdate: string; 
-    products : { "product": Product[] };
-    allunlocks: { "pallier": Pallier[]};
-    upgrades: { "pallier": Pallier[]};
-    angelupgrades: { "pallier": Pallier[]};
-    managers: { "pallier": Pallier[]};
+    private _name: string; 
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+    private _logo: string;
+    public get logo(): string {
+        return this._logo;
+    }
+    public set logo(value: string) {
+        this._logo = value;
+    }
+    private _money: number; 
+    public get money(): number {
+        return this._money;
+    }
+    public set money(value: number) {
+        this._money = value;
+    }
+    private _score: number; 
+    public get score(): number {
+        return this._score;
+    }
+    public set score(value: number) {
+        this._score = value;
+    }
+    private _totalangels: number;
+    public get totalangels(): number {
+        return this._totalangels;
+    }
+    public set totalangels(value: number) {
+        this._totalangels = value;
+    }
+    private _activeangels: number;
+    public get activeangels(): number {
+        return this._activeangels;
+    }
+    public set activeangels(value: number) {
+        this._activeangels = value;
+    }
+    private _angelbonus: number;
+    public get angelbonus(): number {
+        return this._angelbonus;
+    }
+    public set angelbonus(value: number) {
+        this._angelbonus = value;
+    }
+    private _lastupdate: string; 
+    public get lastupdate(): string {
+        return this._lastupdate;
+    }
+    public set lastupdate(value: string) {
+        this._lastupdate = value;
+    }
+    private _products: {
+        "product": Product[];
+    };
+    public get products(): {
+        "product": Product[];
+    } {
+        return this._products;
+    }
+    public set products(value: {
+        "product": Product[];
+    }) {
+        this._products = value;
+    }
+    private _allunlocks: {
+        "pallier": Pallier[];
+    };
+    public get allunlocks(): {
+        "pallier": Pallier[];
+    } {
+        return this._allunlocks;
+    }
+    public set allunlocks(value: {
+        "pallier": Pallier[];
+    }) {
+        this._allunlocks = value;
+    }
+    private _upgrades: {
+        "pallier": Pallier[];
+    };
+    public get upgrades(): {
+        "pallier": Pallier[];
+    } {
+        return this._upgrades;
+    }
+    public set upgrades(value: {
+        "pallier": Pallier[];
+    }) {
+        this._upgrades = value;
+    }
+    private _angelupgrades: {
+        "pallier": Pallier[];
+    };
+    public get angelupgrades(): {
+        "pallier": Pallier[];
+    } {
+        return this._angelupgrades;
+    }
+    public set angelupgrades(value: {
+        "pallier": Pallier[];
+    }) {
+        this._angelupgrades = value;
+    }
+    private _managers: {
+        "pallier": Pallier[];
+    };
+    public get managers(): {
+        "pallier": Pallier[];
+    } {
+        return this._managers;
+    }
+    public set managers(value: {
+        "pallier": Pallier[];
+    }) {
+        this._managers = value;
+    }
 
     constructor() {
         this.products = { "product":[ ] } ;
